5b07df14f4eff31c0276b9768a1e141d86828ed7
Vilya Harvey <vilya.harvey@gmail.com>
2020-01-14 10:59:56 +0000

    Add remaining days to run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -62,3 +62,98 @@ echo "=== Day 10, Part A ==="
 time $BINDIR/day10a $ROOTDIR/day10/input.txt
 echo "=== Day 10, Part B ==="
 time $BINDIR/day10b $ROOTDIR/day10/input.txt
+
+echo ""
+echo "=== Day 10, Part A ==="
+time $BINDIR/day10a $ROOTDIR/day10/input.txt
+echo "=== Day 10, Part B ==="
+time $BINDIR/day10b $ROOTDIR/day10/input.txt
+
+echo ""
+echo "=== Day 11, Part A ==="
+time $BINDIR/day11a $ROOTDIR/day11/input.txt
+echo "=== Day 11, Part B ==="
+time $BINDIR/day11b $ROOTDIR/day11/input.txt
+
+echo ""
+echo "=== Day 12, Part A ==="
+time $BINDIR/day12a $ROOTDIR/day12/input.txt
+echo "=== Day 12, Part B ==="
+time $BINDIR/day12b $ROOTDIR/day12/input.txt
+
+echo ""
+echo "=== Day 13, Part A ==="
+time $BINDIR/day13a $ROOTDIR/day13/input.txt
+echo "=== Day 13, Part B ==="
+time $BINDIR/day13b $ROOTDIR/day13/input.txt
+
+echo ""
+echo "=== Day 14, Part A ==="
+time $BINDIR/day14a $ROOTDIR/day14/input.txt
+echo "=== Day 14, Part B ==="
+time $BINDIR/day14b $ROOTDIR/day14/input.txt
+
+echo ""
+echo "=== Day 15, Part A ==="
+time $BINDIR/day15a $ROOTDIR/day15/input.txt
+echo "=== Day 15, Part B ==="
+time $BINDIR/day15b $ROOTDIR/day15/input.txt
+
+echo ""
+echo "=== Day 16, Part A ==="
+time $BINDIR/day16a $ROOTDIR/day16/input.txt
+echo "=== Day 16, Part B ==="
+time $BINDIR/day16b $ROOTDIR/day16/input.txt
+
+echo ""
+echo "=== Day 17, Part A ==="
+time $BINDIR/day17a $ROOTDIR/day17/input.txt
+echo "=== Day 17, Part B ==="
+time $BINDIR/day17b $ROOTDIR/day17/input.txt
+
+echo ""
+echo "=== Day 18, Part A ==="
+time $BINDIR/day18a $ROOTDIR/day18/input.txt
+echo "=== Day 18, Part B ==="
+time $BINDIR/day18b $ROOTDIR/day18/input.txt
+
+echo ""
+echo "=== Day 19, Part A ==="
+time $BINDIR/day19a $ROOTDIR/day19/input.txt
+echo "=== Day 19, Part B ==="
+time $BINDIR/day19b $ROOTDIR/day19/input.txt
+
+echo ""
+echo "=== Day 20, Part A ==="
+time $BINDIR/day20a $ROOTDIR/day20/input.txt
+echo "=== Day 20, Part B ==="
+time $BINDIR/day20b $ROOTDIR/day20/input.txt
+
+echo ""
+echo "=== Day 21, Part A ==="
+time $BINDIR/day21a $ROOTDIR/day21/input.txt
+echo "=== Day 21, Part B ==="
+time $BINDIR/day21b $ROOTDIR/day21/input.txt
+
+echo ""
+echo "=== Day 22, Part A ==="
+time $BINDIR/day22a $ROOTDIR/day22/input.txt
+echo "=== Day 22, Part B ==="
+time $BINDIR/day22b $ROOTDIR/day22/input.txt
+
+echo ""
+echo "=== Day 23, Part A ==="
+time $BINDIR/day23a $ROOTDIR/day23/input.txt
+echo "=== Day 23, Part B ==="
+time $BINDIR/day23b $ROOTDIR/day23/input.txt
+
+echo ""
+echo "=== Day 24, Part A ==="
+time $BINDIR/day24a $ROOTDIR/day24/input.txt
+echo "=== Day 24, Part B ==="
+time $BINDIR/day24b $ROOTDIR/day24/input.txt
+
+echo ""
+echo "=== Day 25, Part A ==="
+time $BINDIR/day25a $ROOTDIR/day25/input.txt
+
