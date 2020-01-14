e009f7c4287b03a122d087e05218aa265b9bea30
Vilya Harvey <vilya.harvey@gmail.com>
2020-01-14 10:43:43 +0000

    Improve performance of day05b
    Time went from around 6.7 seconds to 0.29 seconds.
    
    Algorithmic improvement was noticing that if our first reacting pair is at position i, then the earliest possible reacting pair after removing them is at i-1 so we don't need to search from the beginning of the string after each reaction.
    
    Also I'm now using a std::string instead of a linked list of chars; and call str.erase() to remove reacting pairs. Not sure whether this contributed to the perf wins, but it did simplify things.

--- day05/day05b.cpp
+++ day05/day05b.cpp
@@ -1,7 +1,45 @@
+#include <cctype>
 #include <cstdio>
 #include <limits>
+#include <string>
 
 static constexpr int kSize = 50000;
+
+
+std::string remove_polymer(const std::string& src, char polymer, std::string& dst)
+{
+  polymer |= 32;
+
+  dst.clear();
+  dst.reserve(src.size());
+
+  for (char ch : src) {
+    if ((ch | 32) != polymer)
+      dst.push_back(ch);
+  }
+
+  return dst;
+}
+
+
+void react(std::string& chain)
+{
+  size_t i = 1;
+  while (i < chain.size()) {
+    char ch0 = chain[i-1];
+    char ch1 = chain[i];
+    if (ch0 != ch1 && (ch0 | 32) == (ch1 | 32)) {
+      chain.erase(i - 1, 2);
+      i--;
+      if (i == 0)
+        i++;
+    }
+    else {
+      i++;
+    }
+  }
+}
+
 
 int main(int argc, char** argv)
 {
@@ -11,48 +49,20 @@ int main(int argc, char** argv)
   fclose(f);
   line[kSize] = '\0';
 
+  std::string src = line;
+  std::string dst;
+  dst.reserve(src.size());
+
   char best_polymer = '\0';
-  int best_result = std::numeric_limits<int>::max();
-  int first;
-  int next[kSize];
+  size_t best_result = std::numeric_limits<size_t>::max();
   for (char polymer = 'a'; polymer <= 'z'; polymer++) {
-    first = 0;
-    for (int i = 0; i < kSize; i++) {
-      next[i] = i + 1;
-    }
-
-    int n = kSize;
-    for (int *prev = &first, i = 0; i < kSize; i = next[i]) {
-      if ((line[i] | 32) == polymer) {
-        *prev = next[i];
-        n--;
-      }
-      else {
-        prev = &next[i];
-      }
-    }
-
-    bool reacted = true;
-    while (reacted) {
-      reacted = false;
-      for (int *prev = &first, i = first; next[i] < kSize; prev = &next[i], i = next[i]) {
-        char a = line[i];
-        char b = line[next[i]];
-        if (a != b && (a | 32) == (b | 32)) {
-          *prev = next[next[i]];
-          n -= 2;
-          reacted = true;
-          break;
-        }
-        prev = &next[i];
-      }
-    }
-
-    if (n < best_result) {
+    remove_polymer(src, polymer, dst);
+    react(dst);
+    if (dst.size() < best_result) {
       best_polymer = polymer;
-      best_result = n;
+      best_result = dst.size();
     }
   }
 
-  printf("Result = %d\n", best_result);
+  printf("Result = %d\n", int(best_result));
 }
