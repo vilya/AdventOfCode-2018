99b99c562454e0d5fb6d0a0d11bcf677187caebc
Vilya Harvey <vilya.harvey@gmail.com>
2020-01-15 10:09:00 +0000

    Improve performance of my day22b solution
    Instead of caching geologic index, cache the terrain type instead so we don't have to recalculate it every time.
    
    Cache an area of terrain with width and height both equal to the depth of the cave system. This will be large enough that my solution never needs to go outside it, so I can get rid of the std::map holding for outside of the cache.
    
    Instead of using a set to keep track of the visited state for each cell, use an array with the same dimensions as the terrain cache. This array holds a bitset for each cell which says which of the 3 tools we've reached the cell with. This is a *lot* more efficient to get and set than a std::map.
    
    Check whether we've already visited a particular cell with a particular tool *before* adding it to the work queue. Adding an item to the work queue is comparatively expensive, so this saves a fair bit of time.
    
    When switching tools, note that there's only *one* other possibility, not two. Only queue up work for that alternate (and only if we haven't already visited our current cell with that tool). I also noticed that the alternate tool for any given terrain type can be calculated with a simple formula, no lookups or conditionals required.
    
    The runtime for day22b has dropped from 2.3 seconds to 0.6 seconds with these changes.

--- day22/day22b.cpp
+++ day22/day22b.cpp
@@ -13,8 +13,6 @@ struct ivec2 {
 
 bool operator == (ivec2 a, ivec2 b) { return a.y == b.y && a.x == b.x; }
 bool operator != (ivec2 a, ivec2 b) { return a.y != b.y && a.x != b.x; }
-bool operator <  (ivec2 a, ivec2 b) { return (a.y != b.y) ? (a.y <  b.y) : (a.x <  b.x); }
-bool operator >  (ivec2 a, ivec2 b) { return (a.y != b.y) ? (a.y >  b.y) : (a.x >  b.x); }
 
 constexpr ivec2 operator + (ivec2 a, ivec2 b) { return ivec2{ a.x + b.x, a.y + b.y }; }
 constexpr ivec2 operator - (ivec2 a, ivec2 b) { return ivec2{ a.x - b.x, a.y - b.y }; }
@@ -34,8 +32,9 @@ static constexpr int kDepth = 7740;
 static constexpr ivec2 kTarget{ 12, 763 };
 //static constexpr int kDepth = 510;
 //static constexpr ivec2 kTarget{ 10, 10 };
-static constexpr int kWidth = kTarget.x + 1;
-static constexpr int kHeight = kTarget.y + 1;
+
+static constexpr int kWidth = kDepth + 2;
+static constexpr int kHeight = kDepth + 1;
 
 static constexpr int kRocky   = 0;
 static constexpr int kWet     = 1;
@@ -45,98 +44,80 @@ static constexpr int kNeither = 0;
 static constexpr int kTorch = 1;
 static constexpr int kClimbingGear = 2;
 
-
-constexpr int erosion_level(int geologic_index) { return (geologic_index + kDepth) % 20183; }
-
-// terrain == erosion_level % 3
-constexpr bool can_enter_with(int terrain, int tool) { return tool != terrain; }
+static constexpr int kToolPriority[3] = { 2, 0, 1 };
 
 
 struct Work {
   int time, tool;
   ivec2 pos;
 
-  bool operator == (const Work& w) const { return time == w.time && tool == w.tool && pos == w.pos; }
-  bool operator <  (const Work& w) const {
-    // Actually returns > for the benefit of the priority queue.
+  bool operator >  (const Work& w) const {
     if (time != w.time)
       return time > w.time;
-    if (tool != w.tool)
-      return tool > w.tool;
-    return pos > w.pos;
+    return kToolPriority[tool] > kToolPriority[w.tool];
   }
 };
 
 
 struct Map {
-  int g_cache[kHeight][kWidth];
-  std::map<ivec2, int> g_cache_extra;
+  char terrain_cache[kHeight][kWidth];
+  uint8_t visited_cache[kHeight][kWidth];
 
   Map() {
-    g_cache[0][0] = 0;
-    for (int x = 1; x < kWidth; x++)
-      g_cache[0][x] = x * 16807;
-    for (int y = 1; y < kHeight; y++)
-      g_cache[y][0] = y * 48271;
-    g_cache[kTarget.y][kTarget.x] = 0;
+    int erosion[2][kWidth];
+    for (int x = 0; x < kWidth; x++) {
+      erosion[0][x] = (x * 16807 + kDepth) % 20183;
+      terrain_cache[0][x] = erosion[0][x] % 3;
+    }
+    for (int y = 1; y < kHeight; y++) {
+      int curr = y & 1;
+      int prev = curr ^ 1;
+      erosion[curr][0] = (y * 48271 + kDepth) % 20183;
+      terrain_cache[y][0] = erosion[curr][0] % 3;
+      for (int x = 1; x < kWidth; x++) {
+        if (x == kTarget.x && y == kTarget.y)
+          erosion[curr][x] = kDepth;
+        else
+          erosion[curr][x] = (erosion[curr][x-1] * erosion[prev][x] + kDepth) % 20183;
+        terrain_cache[y][x] = erosion[curr][x] % 3;
+      }
+    }
 
-    ivec2 pos;
-    for (pos.y = 1; pos.y < kHeight; pos.y++) {
-      for (pos.x = 1; pos.x < kWidth; pos.x++) {
-        if (pos == kTarget) {
-          continue;
-        }
-        int left = erosion_level(g_cache[pos.y][pos.x - 1]);
-        int up   = erosion_level(g_cache[pos.y - 1][pos.x]);
-        g_cache[pos.y][pos.x] = left * up;
+    for (int y = 0; y < kHeight; y++) {
+      for (int x = 0; x < kWidth; x++) {
+        visited_cache[y][x] = 0;
       }
     }
   }
 
 
-  int get(ivec2 cell) {
-    if (cell.x < kWidth && cell.y < kHeight)
-      return g_cache[cell.y][cell.x];
-    else if (cell.y == 0)
-      return cell.x * 16807;
-    else if (cell.x == 0)
-      return cell.y * 48271;
-
-    auto it = g_cache_extra.find(cell);
-    if (it != g_cache_extra.end())
-      return it->second;
-
-    int g_val = erosion_level(get(cell + kWest)) * erosion_level(get(cell + kNorth));
-    g_cache_extra[cell] = g_val;
-    return g_val;
+  inline bool visited(ivec2 cell, int tool) const {
+    return visited_cache[cell.y][cell.x] & (1u << tool);
   }
 
 
-  int terrain(ivec2 cell) {
-    return erosion_level(get(cell)) % 3;
+  inline void set_visited(ivec2 cell, int tool) {
+    visited_cache[cell.y][cell.x] |= (1u << tool);
   }
 
 
-  bool can_enter(ivec2 cell, int tool) {
-    return terrain(cell) != tool;
+  inline bool can_enter(ivec2 cell, int tool) const {
+    return (terrain_cache[cell.y][cell.x] != tool) && !visited(cell, tool);
   }
 
 
   void print() {
     ivec2 hi = kTarget;
-    for (auto it : g_cache_extra) {
-      hi = max(hi, it.first);
-    }
     const char kErosionLevel[3] = { '.', '=', '|' };
     ivec2 pos;
-    for (pos.y = 0; pos.y <= hi.y; pos.y++) {
-      for (pos.x = 0; pos.x < hi.x; pos.x++) {
+    for (pos.y = 0; pos.y <= hi.y + 5; pos.y++) {
+      for (pos.x = 0; pos.x <= hi.x + 5; pos.x++) {
         if (pos == kStart)
-          fputc('M', stdout);
+          fputc('X', stdout);
         else if (pos == kTarget)
           fputc('T', stdout);
         else
-          fputc(kErosionLevel[erosion_level(get(pos)) % 3], stdout);
+          fputc(kErosionLevel[terrain_cache[pos.y][pos.x]], stdout);
       }
       fputc('\n', stdout);
     }
@@ -146,16 +127,21 @@ struct Map {
 
 
   int quickest_rescue() {
+    // You start with the torch equipped, so the terrain at the starting
+    // position must be something that allows it to be equipped.
+    assert(terrain_cache[0][0] == kRocky || terrain_cache[0][0] == kNarrow);
+
     // BFS where the state is the current position *and* which tool is equipped.
-    std::priority_queue<Work, std::deque<Work>> q;
-    std::set<std::pair<int, ivec2>> visited;
+    std::priority_queue<Work, std::deque<Work>, std::greater<Work>> q;
 
     q.push(Work{0, kTorch, kStart});
     while (!q.empty()) {
       Work work = q.top();
       q.pop();
-      if (!visited.insert(std::make_pair(work.tool, work.pos)).second)
+
+      if (visited(work.pos, work.tool))
         continue;
+      set_visited(work.pos, work.tool);
 
       if (work.pos == kTarget) {
         if (work.tool == kTorch)
@@ -172,10 +158,10 @@ struct Map {
         q.push(Work{work.time + 1, work.tool, work.pos + kEast});
       if (can_enter(work.pos + kSouth, work.tool))
         q.push(Work{work.time + 1, work.tool, work.pos + kSouth});
-      if (can_enter(work.pos, (work.tool + 1) % 3))
-        q.push(Work{work.time + 7, (work.tool + 1) % 3, work.pos});
-      if (can_enter(work.pos, (work.tool + 2) % 3))
-        q.push(Work{work.time + 7, (work.tool + 2) % 3, work.pos});
+
+      int alt_tool = 3 - terrain_cache[work.pos.y][work.pos.x] - work.tool;
+      if (!visited(work.pos, alt_tool))
+        q.push(Work{work.time + 7, alt_tool, work.pos});
     }
 
     return -1;
@@ -185,7 +171,7 @@ struct Map {
 
 int main()
 {
-  Map map;
-//  map.print();
-  printf("Quickest rescue is %d mins\n", map.quickest_rescue());
+  Map* map = new Map();
+//  map->print();
+  printf("Quickest rescue is %d mins\n", map->quickest_rescue());
 }
