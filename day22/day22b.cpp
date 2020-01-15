f46213a055fb3aa8c0ba7ac31196a33c7bc6f111
Vilya Harvey <vilya.harvey@gmail.com>
2020-01-15 12:38:07 +0000

    More performance improvements for day22b
    Limit the cached region to double the target coords and don't queue up work for cells outside this region. This may not be big enough for all possible cases, but is sufficient for my input. This gives an *enormous* speedup.
    
    Prioritise work by distance from the target as well as by time.
    
    Additional smaller improvements:
    - Pack the terrain cache & visited flags into a single byte per cell.
    - Use smaller member data types for ivec2 and Work (int16_t and uint16_t instead of int).
    
    Runtime has dropped from 0.5 seconds to 0.013 seconds!

--- day22/day22b.cpp
+++ day22/day22b.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 struct ivec2 {
-  int x, y;
+  int16_t x, y;
 
   ivec2& operator += (ivec2 v) { x += v.x; y += v.y; return *this; }
 };
@@ -11,8 +11,8 @@ struct ivec2 {
 bool operator == (ivec2 a, ivec2 b) { return a.y == b.y && a.x == b.x; }
 bool operator != (ivec2 a, ivec2 b) { return a.y != b.y && a.x != b.x; }
 
-constexpr ivec2 operator + (ivec2 a, ivec2 b) { return ivec2{ a.x + b.x, a.y + b.y }; }
-constexpr ivec2 operator - (ivec2 a, ivec2 b) { return ivec2{ a.x - b.x, a.y - b.y }; }
+constexpr ivec2 operator + (ivec2 a, ivec2 b) { return ivec2{ int16_t(a.x + b.x), int16_t(a.y + b.y) }; }
+constexpr ivec2 operator - (ivec2 a, ivec2 b) { return ivec2{ int16_t(a.x - b.x), int16_t(a.y - b.y) }; }
 
 ivec2 min(ivec2 a, ivec2 b) { return ivec2{ std::min(a.x, b.x), std::min(a.y, b.y) }; }
 ivec2 max(ivec2 a, ivec2 b) { return ivec2{ std::max(a.x, b.x), std::max(a.y, b.y) }; }
@@ -30,8 +30,8 @@ static constexpr ivec2 kTarget{ 12, 763 };
 //static constexpr int kDepth = 510;
 //static constexpr ivec2 kTarget{ 10, 10 };
 
-static constexpr int kWidth = kDepth + 2;
-static constexpr int kHeight = kDepth + 1;
+static constexpr int kWidth = kTarget.x * 2;
+static constexpr int kHeight = kTarget.y * 2;
 
 static constexpr int kRocky   = 0;
 static constexpr int kWet     = 1;
@@ -45,61 +45,61 @@ static constexpr int kToolPriority[3] = { 2, 0, 1 };
 
 
 struct Work {
-  int time, tool;
+  uint16_t time, tool, dist;
   ivec2 pos;
 
   bool operator >  (const Work& w) const {
     if (time != w.time)
       return time > w.time;
+    if (dist != w.dist)
+      return dist > w.dist;
     return kToolPriority[tool] > kToolPriority[w.tool];
+  }
+
+  static inline Work from(int time, int tool, ivec2 pos) {
+    return Work{ uint16_t(time), uint16_t(tool), uint16_t(std::abs(pos.x - kTarget.x) + std::abs(pos.y - kTarget.y)), pos};
   }
 };
 
 
 struct Map {
-  char terrain_cache[kHeight][kWidth];
-  uint8_t visited_cache[kHeight][kWidth];
+  uint8_t cache[kHeight][kWidth];
 
   Map() {
     int erosion[2][kWidth];
     for (int x = 0; x < kWidth; x++) {
       erosion[0][x] = (x * 16807 + kDepth) % 20183;
-      terrain_cache[0][x] = erosion[0][x] % 3;
+      cache[0][x] = uint8_t((erosion[0][x] % 3) << 6);
     }
     for (int y = 1; y < kHeight; y++) {
       int curr = y & 1;
       int prev = curr ^ 1;
       erosion[curr][0] = (y * 48271 + kDepth) % 20183;
-      terrain_cache[y][0] = erosion[curr][0] % 3;
+      cache[y][0] = uint8_t((erosion[curr][0] % 3) << 6);
       for (int x = 1; x < kWidth; x++) {
         if (x == kTarget.x && y == kTarget.y)
           erosion[curr][x] = kDepth;
         else
           erosion[curr][x] = (erosion[curr][x-1] * erosion[prev][x] + kDepth) % 20183;
-        terrain_cache[y][x] = erosion[curr][x] % 3;
-      }
-    }
-
-    for (int y = 0; y < kHeight; y++) {
-      for (int x = 0; x < kWidth; x++) {
-        visited_cache[y][x] = 0;
+        cache[y][x] = uint8_t((erosion[curr][x] % 3) << 6);
       }
     }
   }
 
 
   inline bool visited(ivec2 cell, int tool) const {
-    return visited_cache[cell.y][cell.x] & (1u << tool);
+    return cache[cell.y][cell.x] & (1u << tool);
   }
 
 
   inline void set_visited(ivec2 cell, int tool) {
-    visited_cache[cell.y][cell.x] |= (1u << tool);
+    cache[cell.y][cell.x] |= (1u << tool);
   }
 
 
   inline bool can_enter(ivec2 cell, int tool) const {
-    return (terrain_cache[cell.y][cell.x] != tool) && !visited(cell, tool);
+    int val = cache[cell.y][cell.x];
+    return ((val >> 6) != tool) && ((val & (1 << tool)) == 0);
   }
 
 
@@ -114,7 +114,7 @@ struct Map {
         else if (pos == kTarget)
           fputc('T', stdout);
         else
-          fputc(kErosionLevel[terrain_cache[pos.y][pos.x]], stdout);
+          fputc(kErosionLevel[cache[pos.y][pos.x] >> 6], stdout);
       }
       fputc('\n', stdout);
     }
@@ -124,14 +124,10 @@ struct Map {
 
 
   int quickest_rescue() {
-    // You start with the torch equipped, so the terrain at the starting
-    // position must be something that allows it to be equipped.
-    assert(terrain_cache[0][0] == kRocky || terrain_cache[0][0] == kNarrow);
-
     // BFS where the state is the current position *and* which tool is equipped.
     std::priority_queue<Work, std::vector<Work>, std::greater<Work>> q;
 
-    q.push(Work{0, kTorch, kStart});
+    q.push(Work::from(0, kTorch, kStart));
     while (!q.empty()) {
       Work work = q.top();
       q.pop();
@@ -143,22 +139,22 @@ struct Map {
       if (work.pos == kTarget) {
         if (work.tool == kTorch)
           return work.time;
-        q.push(Work{work.time + 7, kTorch, kTarget});
+        q.push(Work::from(work.time + 7, kTorch, kTarget));
         continue;
       }
 
       if (work.pos.y > 0 && can_enter(work.pos + kNorth, work.tool))
-        q.push(Work{work.time + 1, work.tool, work.pos + kNorth});
+        q.push(Work::from(work.time + 1, work.tool, work.pos + kNorth));
       if (work.pos.x > 0 && can_enter(work.pos + kWest, work.tool))
-        q.push(Work{work.time + 1, work.tool, work.pos + kWest});
-      if (can_enter(work.pos + kEast, work.tool))
-        q.push(Work{work.time + 1, work.tool, work.pos + kEast});
-      if (can_enter(work.pos + kSouth, work.tool))
-        q.push(Work{work.time + 1, work.tool, work.pos + kSouth});
+        q.push(Work::from(work.time + 1, work.tool, work.pos + kWest));
+      if (work.pos.x < kTarget.x * 2 && can_enter(work.pos + kEast, work.tool))
+        q.push(Work::from(work.time + 1, work.tool, work.pos + kEast));
+      if (work.pos.y < kTarget.y * 2 && can_enter(work.pos + kSouth, work.tool))
+        q.push(Work::from(work.time + 1, work.tool, work.pos + kSouth));
 
-      int alt_tool = 3 - terrain_cache[work.pos.y][work.pos.x] - work.tool;
+      int alt_tool = 3 - (cache[work.pos.y][work.pos.x] >> 6) - work.tool;
       if (!visited(work.pos, alt_tool))
-        q.push(Work{work.time + 7, alt_tool, work.pos});
+        q.push(Work::from(work.time + 7, alt_tool, work.pos));
     }
 
     return -1;
